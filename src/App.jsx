942a82551533338210e94f13ae395f75fc9d3ad1
afridi-cyberpro <afreedmohamed7@gmail.com>
2025-01-22 18:12:13 +0530

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Footer from './components/Footer'
+import Header from './components/header'
 import Nav from './components/Nav'
 function App() {
   return (
     <>
       <Nav/>
       <Footer/>
+      <Header/>
     </>
   )
 }
