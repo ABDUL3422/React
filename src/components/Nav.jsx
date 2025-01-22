b32848533eff17c3557a9d98ba4091898c1adb2f
ABDUL3422 <itrish34229@gmail.com>
2025-01-22 17:29:22 +0530

    1st

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div class="navbar">
+        <h1 class="logo">Diagano.Ai</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
